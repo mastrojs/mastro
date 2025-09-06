659c3c38838564d72ed3689cee7e57b5679e733b
mb21 <mb21@users.noreply.github.com>
2025-09-06 09:40:25 +0200

    v0.2.1

--- examples/todo-list-server/routes/index.server.ts
+++ examples/todo-list-server/routes/index.server.ts
@@ -11,7 +11,7 @@ export const GET = async () => {
       <script type="importmap">
       {
         "imports": {
-          "mastro/reactive": "https://esm.sh/jsr/@mastrojs/mastro@0.1.3/reactive?bundle"
+          "mastro/reactive": "https://esm.sh/jsr/@mastrojs/mastro@0.2.1/reactive?bundle"
         }
       }
       </script>
