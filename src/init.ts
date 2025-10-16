6ecd5f41b065a06770bef3fd9c2f331d603fbe3a
mb21 <mb21@users.noreply.github.com>
2025-10-16 17:27:43 +0200

    Make init node-compatible

--- src/init.ts
+++ src/init.ts
@@ -4,37 +4,63 @@
  * @module
  */
 
-const templateRepo = "template-basic-deno";
-const dir = prompt("What folder should we create for your new project?");
+import { exec } from "node:child_process";
+import fs from "node:fs/promises";
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline/promises";
+import { writeFile } from "./node/writeFile.ts";
+
+const repoName = `template-basic-${typeof Deno === "object" ? "deno" : "node"}`;
+const repoUrl = `https://github.com/mastrojs/${repoName}/archive/refs/heads/main.zip`;
+const zipFilePromise = fetch(repoUrl);
+
+const rl = createInterface({ input: stdin, output: stdout, crlfDelay: Infinity });
+const dir = await rl.question("What folder should we create for your new project?\n");
+rl.close();
+stdin.destroy();
+
+const execCmd = (
+  cmd: string,
+): Promise<{ code: number; stdout: string; stderr: string }> =>
+  new Promise((resolve) =>
+    exec(cmd, (error, stdout, stderr) =>
+      resolve({
+        code: error ? (error.code || -1) : 0,
+        stdout,
+        stderr,
+      }))
+  );
 
 if (dir) {
-  // download zip file from GitHub
-  const res = await fetch(`https://github.com/mastrojs/${templateRepo}/archive/refs/heads/main.zip`);
-  const outDir = templateRepo + "-main"; // this cannot be changed and is determined by the zip file
-  const zipFile = outDir + ".zip";
+  const outDir = repoName + "-main"; // this cannot be changed and is determined by the zip file
+  const zipFileName = outDir + ".zip";
+  const res = await zipFilePromise;
   if (res.ok && res.body) {
-    await Deno.writeFile(zipFile, res.body);
+    await writeFile(zipFileName, res.body);
   }
 
-  // unzip (should work on unix and Windows 10 and later)
-  const command = new Deno.Command("tar", { args: ["-xf", zipFile] });
-  const { code, stdout, stderr } = await command.output();
+  // unzip using the tar command (should work on unix and Windows 10 and later)
+  const { code, stdout, stderr } = await execCmd(`tar -xf ${zipFileName}`);
   const unzipSuccess = code === 0;
   if (!unzipSuccess) {
-    console.log(new TextDecoder().decode(stdout));
-    console.error(new TextDecoder().decode(stderr));
+    console.log(stdout);
+    console.error(stderr);
   }
-  await Deno.remove(zipFile);
+  await fs.rm(zipFileName, { force: true, recursive: true });
 
   if (unzipSuccess) {
-    await Deno.rename(outDir, dir);
+    await fs.rename(outDir, dir);
 
     const codeStyle = "color: blue";
-    console.log(`
+    console.log(
+      `
 Success!
 
 Enter your project directory using %ccd ${dir}
-%cThen start the dev server with: %cdeno task start`
-    , codeStyle, "", codeStyle);
+%cThen start the dev server with: %cdeno task start`,
+      codeStyle,
+      "",
+      codeStyle,
+    );
   }
 }
