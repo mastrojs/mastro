a60448e0a09930d6f15b84067475eb73fcdf51b9
mb21 <mb21@users.noreply.github.com>
2025-12-15 09:43:00 +0100

    Update examples/todo-list

--- examples/todo-list-server/routes/todo/[id].server.ts
+++ examples/todo-list-server/routes/todo/[id].server.ts
@@ -3,7 +3,7 @@ import * as db from "../../models/todo.ts";
 
 export const PATCH = async (req: Request) => {
   const { done, title } = await req.json();
-  const { id } = getParams(req.url);
+  const { id } = getParams(req);
   if (!id) {
     return jsonResponse({ error: "Not found" }, 404);
   } else {
