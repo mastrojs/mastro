8af4b7c8c1d30a14b3397dfc9b1fa9b3dca51ef1
mb21 <mb21@users.noreply.github.com>
2026-03-01 20:12:32 +0100

    Use stripTypeScriptTypes in Node.js

--- src/staticFiles.ts
+++ src/staticFiles.ts
@@ -4,21 +4,25 @@ import { staticCacheControlVal } from "./server.ts";
 const importRegex = /^import .*\.ts("|')(;)?$/gm;
 
 /**
- * Convert a TypeScript string to JavaScript by running it through `tsBlankSpace`
- * and changing imports ending with `.ts` to end with `.js`.
+ * Convert a TypeScript string to JavaScript by running it through `stripTypeScriptTypes` (Node.js)
+ * and `ts-blank-space` (Deno and Bun), and then changing imports ending with `.ts` to end in `.js`.
  *
  * Since browsers don't understand TypeScript and static file servers generally
  * don't serve `.ts` files with `content-type: text/javascript`, we need to run this
  * function on `.client.ts` files, to convert them to `.client.js` files.
  */
-export const tsToJs = (text: string): Promise<string> =>
-  import("ts-blank-space").then((tsBlankSpace) =>
-    tsBlankSpace.default(text)
-      .replace(
-        importRegex,
-        (match, quote, semicolon) => match.slice(0, semicolon ? -5 : -4) + `.js${quote};`,
-      )
+export const tsToJs = async (code: string): Promise<string> => {
+  const tsBlankSpace = ["npm", "ts-blank-space"].join(":");
+  // @ts-expect-error no type definitions for Bun
+  const { stripTypeScriptTypes } = typeof Deno === "object" || typeof Bun === "object"
+    ? await import(tsBlankSpace).then((m) => ({ stripTypeScriptTypes: m.default }))
+    : await import("node:module");
+  const js: string = stripTypeScriptTypes(code);
+  return js.replace(
+    importRegex,
+    (match, quote, semicolon) => match.slice(0, semicolon ? -5 : -4) + `.js${quote};`,
   );
+};
 
 /**
  * Utility function for the server to serve static files as well.
